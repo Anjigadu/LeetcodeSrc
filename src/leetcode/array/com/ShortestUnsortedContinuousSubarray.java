8ab58a4ca0ec068dccdd47c55fd1b3886d35c6c7
prakash <www.prak.co.in@gmail.com>
2017-07-18 18:29:38 +0530

    solution from net

--- src/leetcode/array/com/ShortestUnsortedContinuousSubarray.java
+++ src/leetcode/array/com/ShortestUnsortedContinuousSubarray.java
@@ -52,6 +52,33 @@ public class ShortestUnsortedContinuousSubarray {
         }
 	}
 	
+	/*
+	 * Solution from net.
+	 * I use the variables beg and end to keep track of minimum subarray A[beg...end] 
+	 * which must be sorted for the entire array A to be sorted. 
+	 * If end < beg < 0 at the end of the for loop, then the array is already fully sorted.
+	 * https://discuss.leetcode.com/topic/89282/java-o-n-time-o-1-space/2
+	 */
+	public static int findUnsortedSubarray(int[] A) {
+	    int n = A.length;
+	    int beg = -1; 
+	    int end = -2; 
+	    int min = A[n-1]; 
+	    int max = A[0];
+	    for (int i=1;i<n;i++) {
+	      max = Math.max(max, A[i]); //from left to right, search the current max
+	      min = Math.min(min, A[n-1-i]); //from right to left, search the current min 
+	      if (A[i] < max){
+	    	  end = i;
+	      }
+	      if (A[n-1-i] > min){ 
+	    	  beg = n-1-i; 
+	      }
+	    }
+	    System.out.println("lastIndex value: " + end + ", firstIndex value: " + beg );
+	    return end - beg + 1;
+	}
+	
 	public static void main(String []args){
 		//int []nums = {1,2,3,3,3};
 		//int []nums = {1,3,2,2,2};
@@ -60,6 +87,7 @@ public class ShortestUnsortedContinuousSubarray {
 		//int [] nums = {5,4,3,2,1};
 		int [] nums = {2,3,3,2,4};
 		System.out.println(shortestUnsortedContinuousSubarray(nums));
+		System.out.println(findUnsortedSubarray(nums));
 	}
 
 }
