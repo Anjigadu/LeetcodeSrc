d4dabc21fb557a081e5f111b40d8fdf668e4e6ea
prakash <www.prak.co.in@gmail.com>
2017-07-18 07:31:23 +0530

    More Efficient way to remove duplicate.

--- src/leetcode/array/com/RemoveDuplicatesFromSortedArray.java
+++ src/leetcode/array/com/RemoveDuplicatesFromSortedArray.java
@@ -30,6 +30,34 @@ public class RemoveDuplicatesFromSortedArray {
 	    return i;
     }
 	
+	
+	/*
+	 *   Throws an exception if values are not in the range of 0-1000
+	 */
+	
+	
+	public static int[] removeDuplicatesInGivenRange(int[] arr) {
+	    boolean[] set = new boolean[1001]; //values must default to false
+	    int totalItems = 0;
+
+	    for (int i = 0; i < arr.length; ++i) {
+	        if (!set[arr[i]]) {
+	            set[arr[i]] = true;
+	            totalItems++;
+	        }
+	    }
+
+	    int[] ret = new int[totalItems];
+	    int c = 0;
+	    for (int i = 0; i < set.length; ++i) {
+	        if (set[i]) {
+	            ret[c++] = i;
+	        }
+	    }
+	    return ret;
+	}
+	
+	
 	public static void main(String []args)
 	{
 		int [] array = {1,1,2};
