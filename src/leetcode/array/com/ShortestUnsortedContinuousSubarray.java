1f68052769162e24b4ee5be8caaf32eab963e0d0
prakash <www.prak.co.in@gmail.com>
2017-07-18 18:19:19 +0530

    Not fully working code.

--- src/leetcode/array/com/ShortestUnsortedContinuousSubarray.java
+++ src/leetcode/array/com/ShortestUnsortedContinuousSubarray.java
@@ -1,7 +1,7 @@
 package leetcode.array.com;
 
 /*
- * https://leetcode.com/problems/shortest-unsorted-continuous-subarray/#/hints
+ * https://leetcode.com/problems/shortest-unsorted-continuous-subarray/#hints
 Given an integer array, you need to find one continuous subarray that if you 
 only sort this subarray in ascending order, then the whole array will be sorted 
 in ascending order, too.
@@ -23,5 +23,43 @@ The input array may contain duplicates, so ascending order here means <=.
  */
 
 public class ShortestUnsortedContinuousSubarray {
+	
+	
+	public static int shortestUnsortedContinuousSubarray(int [] arr){
+		int firstIndex = 0;
+		int lastIndex = 0;
+		boolean flag = true;
+		for(int i = 0; i< arr.length -1 ; i++){
+				if(flag){
+					if(arr[i] > arr[i+1] || arr[firstIndex] > arr[i+1]){
+						firstIndex = i;
+						flag =  false;
+						lastIndex = i+1;
+					}
+				}else{
+					if(arr[i] > arr[i+1] || arr[firstIndex] > arr[i+1]){
+						lastIndex = i+1;
+					}
+				}
+				
+		}
+		
+		System.out.println("lastIndex value: " + lastIndex + ", firstIndex value: " + firstIndex );
+		if(lastIndex != 0 || firstIndex != 0){
+            return  lastIndex - firstIndex +1;
+        }else{
+            return 0;
+        }
+	}
+	
+	public static void main(String []args){
+		//int []nums = {1,2,3,3,3};
+		//int []nums = {1,3,2,2,2};
+		//int [] nums = {2,6,4,8,10,9,15};
+		//int [] nums = {2,1};
+		//int [] nums = {5,4,3,2,1};
+		int [] nums = {2,3,3,2,4};
+		System.out.println(shortestUnsortedContinuousSubarray(nums));
+	}
 
 }
